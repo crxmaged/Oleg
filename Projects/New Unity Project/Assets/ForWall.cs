da6251eb1ea13658588fa2f9cbec8df4f3b45892
Oleh Khlopov <crxmaged@gmail.com>
2019-03-27 09:23:47 +0200

    unit+MVP

---
+++ Projects/New Unity Project/Assets/ForWall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.G))
+		{
+			GetComponent<Renderer>().material.color = Color.gray;
+		}
+		else if (Input.GetKey(KeyCode.W))
+		{
+			GetComponent<Renderer>().material.color = Color.white;
+		}
+		else if (Input.GetKey(KeyCode.M))
+		{
+			GetComponent<Renderer>().material.color = Color.magenta;
+		}
+		else
+		{
+			GetComponent<Renderer>().material.color = Color.black;
+		}
+	}
+}
