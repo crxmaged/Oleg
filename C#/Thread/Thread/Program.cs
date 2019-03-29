8e4cb77b09011e0f8afdce52f50513a1568f1523
Oleh Khlopov <crxmaged@gmail.com>
2019-03-29 17:28:45 +0200

    Thread

---
+++ C#/Thread/Thread/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTask
+{
+	class Program
+	{
+		static void Second()
+		{
+			while (true)
+			{
+				Console.WriteLine("                     Second thread!");
+				Thread.Sleep(200);
+			}
+		}
+
+
+		static void Main(string[] args)
+		{
+			ThreadStart writeSecond = new ThreadStart(Second);
+			Thread thread = new Thread(writeSecond);
+			thread.Start();
+
+			for (int i = 0; i < 2; i++)
+			{
+				Console.WriteLine("Prime thread");
+				Thread.Sleep(200);
+			}
+
+			thread.IsBackground = true;
+
+			Console.ReadLine();
+		}
+
+	}
+}
