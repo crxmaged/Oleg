1d66ca09456e535c700dd48bd3fddaf5c2fb6e8c
Oleh Khlopov <crxmaged@gmail.com>
2019-04-05 16:27:21 +0300

    camelCase

---
+++ C#/CamelCase/CamelCase/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CamelCase
+{
+	class Program
+	{ // most terable code
+		public static string ToCamelCase(string str)
+		{
+			string newStr = null, camelCase = null;
+			//str.ToLower();
+			string[] stringArray = str.Split('-', '_', ' ', ',', ':', '*', '%', '+', '=', '`', '&', '^', '>', '<', '/', '#');
+			for (int i = 0; i < stringArray.Length; i++)
+			{
+				string[] elementOfarrayUP = new string [stringArray.Length];
+				string[] elementOfarrayLow = new string[stringArray.Length];
+				elementOfarrayUP[i] = stringArray[i].ToUpper();
+				elementOfarrayLow[i] = stringArray[i].ToLower();
+				var charArray = stringArray[i].ToCharArray();
+				var charArreyUp = elementOfarrayUP[i].ToCharArray();
+				var charArreyLow = elementOfarrayLow[i].ToCharArray();
+        
+				for (int n = 0; n < charArreyUp.Length; n++)
+				{
+					if (i == 0)
+					{
+						charArreyLow[0] = charArray[0];
+					}
+					else
+					{
+						charArreyLow[0] = charArreyUp[0];
+					}
+				}
+				newStr = new string(charArreyLow);
+				stringArray[i] = newStr;
+				camelCase = camelCase + string.Join("", newStr);
+			}
+			return camelCase;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(ToCamelCase("The_stealth_warrior"));
+			Console.ReadKey();
+		}
+	}
+}
