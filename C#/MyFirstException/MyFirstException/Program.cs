5cd3a6c856a35ffe81dcf513dbcfcc9947bd50ec
Oleh Khlopov <crxmaged@gmail.com>
2019-04-16 15:32:25 +0300

    add prog - show how works exception

---
+++ C#/MyFirstException/MyFirstException/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstException
+{
+	public class MYException : Exception // Creat personal exception
+	{
+		public void Method() 
+		{
+			Console.WriteLine("User Exception!");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{ // operator throw call Exception
+				throw new MYException(); // Call user exception MYException without bool 
+			}
+
+			catch (MYException e)
+			{
+				Console.WriteLine("Use user exception!"); // Show exception 
+				e.Method();
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
