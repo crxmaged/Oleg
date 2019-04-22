1b03e8234cbb246cfe0fd244da1d69d8b6fef7d3
Oleh Khlopov <crxmaged@gmail.com>
2019-04-22 17:32:05 +0300

    mirror

---
+++ C#/Mirror/Mirror/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mirror
+{
+	class Program
+	{
+		public int ReverseNumber(int n)
+		{
+			char[] digits = n.ToString().ToCharArray();
+			char[] firstNum = digits;
+			char thx;
+			for (int i = 0; i < digits.Length/2; i++)
+			{
+				for (int k = digits.Length - 1 - i; k == digits.Length - 1 - i; k--)
+				{
+					thx = digits[i];
+					digits[i] = firstNum[k];
+					digits[k] = thx;
+				}
+			}
+			string s = new string(digits);
+			n = Int32.Parse(s);
+			return n;
+		}
+
+		static void Main(string[] args)
+		{
+			Program newEx = new Program();
+
+			Console.WriteLine("{0}", newEx.ReverseNumber(12345678));
+
+			Console.ReadKey();
+		}
+	}
+}
