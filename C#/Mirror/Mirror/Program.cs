0a3b776c08c36ffe35bac56b62fbbceff549334e
Oleh Khlopov <crxmaged@gmail.com>
2019-04-23 09:38:15 +0300

    finished work

--- C#/Mirror/Mirror/Program.cs
+++ C#/Mirror/Mirror/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mirror
 {
@@ -10,28 +6,54 @@ namespace Mirror
 	{
 		public int ReverseNumber(int n)
 		{
-			char[] digits = n.ToString().ToCharArray();
-			char[] firstNum = digits;
+			char[] digits = null;
+			char[] firstNum = null;
 			char thx;
-			for (int i = 0; i < digits.Length/2; i++)
+			string s;
+			if (n < 0)
 			{
-				for (int k = digits.Length - 1 - i; k == digits.Length - 1 - i; k--)
+				n = n * (-1);
+				digits = n.ToString().ToCharArray();
+				firstNum = digits;
+				for (int i = 0; i < digits.Length / 2; i++)
 				{
-					thx = digits[i];
-					digits[i] = firstNum[k];
-					digits[k] = thx;
+					for (int k = digits.Length - 1 - i; k == digits.Length - 1 - i; k--)
+					{
+						thx = digits[i];
+						digits[i] = firstNum[k];
+						digits[k] = thx;
+					}
 				}
+				s = new string(digits);
+				n = Int32.Parse(s);
+				return n * (-1);
 			}
-			string s = new string(digits);
-			n = Int32.Parse(s);
-			return n;
+			else
+			{
+				digits = n.ToString().ToCharArray();
+				firstNum = digits;
+				for (int i = 0; i < digits.Length / 2; i++)
+				{
+					for (int k = digits.Length - 1 - i; k == digits.Length - 1 - i; k--)
+					{
+						thx = digits[i];
+						digits[i] = firstNum[k];
+						digits[k] = thx;
+					}
+				}
+				s = new string(digits);
+				n = Int32.Parse(s);
+				return n;
+			}
 		}
+
+		// return int.Parse(String.Join("", Math.Abs(n).ToString().Reverse())) * (n < 0 ? -1 : 1);
 
 		static void Main(string[] args)
 		{
 			Program newEx = new Program();
 
-			Console.WriteLine("{0}", newEx.ReverseNumber(12345678));
+			Console.WriteLine("{0}", newEx.ReverseNumber(-12345678));
 
 			Console.ReadKey();
 		}
