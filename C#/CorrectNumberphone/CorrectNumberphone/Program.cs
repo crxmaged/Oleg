f1477d6151705b64dd15506bb749cc803c7f7db9
Oleh Khlopov <crxmaged@gmail.com>
2019-04-26 15:06:45 +0300

    improve code

--- C#/CorrectNumberphone/CorrectNumberphone/Program.cs
+++ C#/CorrectNumberphone/CorrectNumberphone/Program.cs
@@ -1,43 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CorrectNumberphone
 {
 	class Program
 	{ // "(123) 456-7890" - true
+		public bool CheckDigit (char [] inArrayChar)
+		{
+			if (Char.IsDigit(inArrayChar[1]) && Char.IsDigit(inArrayChar[2]) && Char.IsDigit(inArrayChar[3]) && Char.IsDigit(inArrayChar[6]) && Char.IsDigit(inArrayChar[7])
+						&& Char.IsDigit(inArrayChar[8]) && Char.IsDigit(inArrayChar[10]) && Char.IsDigit(inArrayChar[11]) && Char.IsDigit(inArrayChar[12]) && Char.IsDigit(inArrayChar[13]))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
 		public bool PhoneNumber(string number)
 		{
-			// int n;
 			bool ans = false;
 			char[] arrayChar = number.ToCharArray();
-			if (arrayChar.Length == 14)
+			if (arrayChar.Length == 14 && arrayChar[0] == '(' && arrayChar[4] == ')' && arrayChar[5] == ' ' && arrayChar[9] == '-')
 			{
-				if (arrayChar[0] == '(' && arrayChar[4] == ')' && arrayChar[5] == ' ' && arrayChar[9] == '-')
-				{
-					if (Char.IsDigit(arrayChar[1]) && Char.IsDigit(arrayChar[2]) && Char.IsDigit(arrayChar[3]) && Char.IsDigit(arrayChar[6]) && Char.IsDigit(arrayChar[7]) 
-						&& Char.IsDigit(arrayChar[8]) && Char.IsDigit(arrayChar[10]) && Char.IsDigit(arrayChar[11]) && Char.IsDigit(arrayChar[12]) && Char.IsDigit(arrayChar[13]))
-					{
-						ans = true;
-					}
-				}
-				/*
-				for (int i = 0; i < arrayChar.Length; i++)
-				{
-					switch (arrayChar[i])
-					{
-						case '(':
-							break;
-
-					}
-				}
-				*/
+				ans = true;
 			}
-
+			else
+			{
+				ans = false;
+			}
 			return ans;
-		}
+		} 
 
 		static void Main(string[] args)
 		{
