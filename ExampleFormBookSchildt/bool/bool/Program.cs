9db87a4af75447e59d393f2cf6d0eec910a125fd
Oleg <crxmaged@gmail.com>
2019-08-04 23:40:00 +0300

    add bool simple example

---
+++ ExampleFormBookSchildt/bool/bool/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @bool
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i, j;
+			bool b1, b2;
+			i = 10; j = 11;
+			if (i < j) Console.WriteLine(" i < j");
+			if (i <= j) Console.WriteLine(" i <= j");
+			if (i != j) Console.WriteLine(" i != j");
+			if (i == j) Console.WriteLine(" No execution ");
+			if (i >= j) Console.WriteLine(" No execution ");
+			if (i > j) Console.WriteLine(" No execution ");
+
+			b1 = true; b2 = false;
+
+			if (b1 & b2) Console.WriteLine(" No execution ");
+			if (!(b1 & b2)) Console.WriteLine(" !(b1 & b2) - true ");
+			if (b1 | b2) Console.WriteLine(" b1 | b2 - true ");
+			if (b1 ^ b2) Console.WriteLine(" b1 ^ b2 - true ");
+
+			Console.ReadLine();
+
+		}
+	}
+}
