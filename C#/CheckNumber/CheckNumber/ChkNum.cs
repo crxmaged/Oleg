09514fcef521e96064858cc01cc6224288848880
Oleh Khlopov <crxmaged@gmail.com>
2019-09-04 17:19:00 +0300

    add program for check numbers from 2 to 10 Prime

---
+++ C#/CheckNumber/CheckNumber/ChkNum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNumber
+{
+	class ChkNum
+	{
+		public bool IsPrime (int x)
+		{
+			if (x <= 1) return false;
+			for (int i = 2; i <= x / i; i++) // go anothe step if don't work return on 16 line
+			{
+				if ((x % i) == 0) return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ C#/CheckNumber/CheckNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNumber
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ChkNum ob = new ChkNum();
+
+			for (int i = 2; i < 10; i++)
+			{
+				if (ob.IsPrime(i)) Console.WriteLine(i + " simple number");
+				else Console.WriteLine(i + " not simple number");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
