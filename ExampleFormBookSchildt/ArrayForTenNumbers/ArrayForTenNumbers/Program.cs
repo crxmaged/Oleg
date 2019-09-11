8fdf4c5844405fd065a9090a3f87489c928716f6
Oleh Khlopov <crxmaged@gmail.com>
2019-09-11 16:47:57 +0300

    array

---
+++ ExampleFormBookSchildt/ArrayForTenNumbers/ArrayForTenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArrayForTenNumbers
+{
+	class Program
+	{ // simple example work with array
+		static void Main(string[] args)
+		{
+			int[] intArray = new int[10]; // array
+
+			for (int i = 0; i < intArray.Length; i++)
+			{
+				intArray[i] = i + 1; // cycle to add elements to array
+			}
+
+			foreach (int element in intArray)
+			{
+				Console.Write("number: " + element + " "); // write to console all elements array
+			}
+			Console.ReadLine();
+		}
+	}
+}
