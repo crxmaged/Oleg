b27fce21bf7c83b80bee399e7a0df0da5bf2674e
Oleh Khlopov <crxmaged@gmail.com>
2019-09-24 10:48:13 +0300

    array var

---
+++ ExampleFormBookSchildt/VarJaggedArray/VarJaggedArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarJaggedArray
+{
+	class Program
+	{ // Example array (jagged) with var elements
+		static void Main(string[] args)
+		{
+			var jagged = new[] { new[] { 1, 2, 3, 4 }, new[] { 9, 8, 7 }, new[] { 11, 12, 13, 14, 15 } };
+			for (int j = 0; j < jagged.Length; ++j) // jagged.Length = 3
+			{
+				for (int i = 0; i < jagged[j].Length; i++) // jagged[0].Length = 4, jagged[1].Length = 3, jagged[2].Length = 5
+				{
+					Console.Write("{0} ", jagged[j][i]);
+					Console.WriteLine();
+				}
+			}
+			Console.ReadLine();
+		}
+	}
+}
