4d39e72f0401fd539eaa0d78558db04fcee7357f
Oleg <crxmaged@gmail.com>
2019-11-05 20:11:08 +0200

    add

---
+++ ExampleFormBookSchildt/UseIncopsulaton/UseIncopsulaton/FolderWithClass/Stack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseIncopsulaton.FolderWithClass
+{
+	class Stack
+	{
+		char[] strk; // mass like a stack
+		int tos; // end of stack
+
+		public Stack()
+		{
+
+		}
+		public Stack (int size)
+		{
+			strk = new char[size]; // instance
+			tos = 0;
+		}
+
+		public void Push (char ch)
+		{
+			if (tos == strk.Length)
+			{
+				Console.WriteLine("Stack is full");
+				return;
+			}
+			strk[tos] = ch; tos++;
+		}
+
+		public char Pop ()
+		{
+			if (tos == 0)
+			{
+				Console.WriteLine("Stack is empty!");
+				return (char)0;
+			}
+			tos--;
+			return strk[tos];
+		}
+
+		public bool IsFull()
+		{
+			return tos == strk.Length;
+		}
+
+		public bool IsEmpty()
+		{
+			return tos == 0;
+		}
+
+		public int Capacity()
+		{
+			return strk.Length;
+		}
+
+		public int GetNun()
+		{
+			return tos;
+		}
+	}
+}
