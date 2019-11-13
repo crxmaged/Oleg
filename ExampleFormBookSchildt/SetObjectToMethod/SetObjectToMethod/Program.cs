d3884b58c890d0254781d1a4a33fd9c06bb37f39
Oleh Khlopov <crxmaged@gmail.com>
2019-11-14 00:08:54 +0200

    new obj

---
+++ ExampleFormBookSchildt/SetObjectToMethod/SetObjectToMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SetObjectToMethod.classStore;
+
+namespace SetObjectToMethod
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			UserClass user1 = new UserClass(1, 10);
+			UserClass user2 = new UserClass(22, 33);
+
+			Console.WriteLine("Are the object {0}, are same {1}? ans: {2}", nameof(user1), nameof(user2), user1.AsSame(user2));
+			user1.Show();
+			user2.Show();
+			Console.WriteLine("Value object user2 to object user1");
+			user1.Copy(user2);
+			user1.Show();
+			user2.Show();
+			Console.WriteLine("Are the object {0}, are same {1}? ans: {2}", nameof(user1), nameof(user2), user1.AsSame(user2));
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ ExampleFormBookSchildt/SetObjectToMethod/SetObjectToMethod/classStore/UserClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetObjectToMethod.classStore
+{
+	class UserClass // In this class we have method that get object type
+	{
+		int alpha, beta;
+
+		public UserClass()
+		{
+		}
+
+		public UserClass(int i, int j)
+		{
+			alpha = i;
+			beta = j;
+		}
+
+		public bool AsSame (UserClass obClass)
+		{
+			if ((obClass.alpha == alpha) & (obClass.beta == beta))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public void Copy (UserClass userClass)
+		{
+			alpha = userClass.alpha;
+			beta = userClass.beta;
+		}
+
+		public void Show()
+		{
+			Console.WriteLine("alpha: {0}, beta {1}", alpha, beta);
+		}
+	}
+}
