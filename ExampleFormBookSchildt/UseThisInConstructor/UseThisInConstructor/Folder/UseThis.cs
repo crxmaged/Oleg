609d1929dc1231aeedfd735a81310060e77f1ecb
Oleh Khlopov <crxmaged@gmail.com>
2020-01-16 10:31:14 +0200

    use this in constructor

---
+++ ExampleFormBookSchildt/UseThisInConstructor/UseThisInConstructor/Folder/UseThis.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UseThisInConstructor.Folder
+{
+	class UseThis
+	{
+		public int x, y;
+		 // First do this after constructor where this stand
+		public UseThis () : this(0, 0)
+		{
+			Console.WriteLine("Default constructor!");
+		}
+
+		public UseThis (UseThis obj): this (obj.x, obj.y)
+		{
+			Console.WriteLine("Constructor with argument obj type UseThis");
+		}
+
+		public UseThis (int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+			Console.WriteLine("Constructor where set value variable x: {0} and y: {1}", x, y);
+		}
+	}
+}
---
+++ ExampleFormBookSchildt/UseThisInConstructor/UseThisInConstructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UseThisInConstructor.Folder;
+
+namespace UseThisInConstructor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Call different constructor
+			UseThis newObj1 = new UseThis();
+			UseThis newObj2 = new UseThis(10, 11);
+			UseThis newObj3 = new UseThis(newObj2);
+
+			Console.ReadLine();
+		}
+	}
+}
