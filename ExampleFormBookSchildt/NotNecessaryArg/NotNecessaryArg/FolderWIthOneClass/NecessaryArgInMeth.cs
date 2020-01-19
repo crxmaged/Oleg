651767a207caa9c2e40d61163b8c9f54bdfc97d1
Oleh Khlopov <crxmaged@gmail.com>
2020-01-19 21:54:32 +0200

    creat method with original arguments

---
+++ ExampleFormBookSchildt/NotNecessaryArg/NotNecessaryArg/FolderWIthOneClass/NecessaryArgInMeth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotNecessaryArg.FolderWIthOneClass
+{
+	class NecessaryArgInMeth
+	{
+		public static void CallTheMehod(int age, int id = 100000001, string name = "No set perosnal name")
+		{
+			Console.WriteLine("Age person: {0}",  age);
+			Console.WriteLine("Id person: {0}", id);
+			Console.WriteLine("Name person: {0}", name);
+		}
+	}
+}
---
+++ ExampleFormBookSchildt/NotNecessaryArg/NotNecessaryArg/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NotNecessaryArg.FolderWIthOneClass;
+
+namespace NotNecessaryArg
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Now we call method with Original arguments
+			NecessaryArgInMeth.CallTheMehod(33, 01, "Neel");
+			NecessaryArgInMeth.CallTheMehod(22, 02);
+			NecessaryArgInMeth.CallTheMehod(50);
+
+			Console.ReadLine();
+		}
+	}
+}
