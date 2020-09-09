fb874c9bb1a9d0ac24079e92d57db70f9ebb3180
Oleh Khlopov <crxmaged@gmail.com>
2020-09-09 15:30:35 +0300

    som fix

---
+++ ExampleFormBookSchildt/ShortProgram/ShortProgram/Folder/ClassWithAutoProperty.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ShortProgram.Folder
+{
+	class ClassWithAutoProperty
+	{ // auto propertes with short and simple get and set method
+		public int Num { get; set; } // property Num
+		public string Text { get; set; } // property Text
+	}
+}
---
+++ ExampleFormBookSchildt/ShortProgram/ShortProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ShortProgram.Folder;
+
+namespace ShortProgram
+{
+	class Program
+	{ // page 320 
+		static void Main(string[] args)
+		{ // Use property and short way to set value 
+			ClassWithAutoProperty exm = new ClassWithAutoProperty { Num = 11011, Text = "WTF?"};
+			// Then we get value and show on console 
+			Console.WriteLine("Show what have class! Text: {0} and number: {1}", exm.Text, exm.Num);
+
+			Console.ReadLine();
+		}
+	}
+}
